633ce58113d61d96a7a381fa43d476ebfaf1af23
jthach <jthach@iu.edu>
2022-12-25 01:54:42 -0800

    done

---
+++ Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Hi, I'm Janet</h1>
+      <p>I'm looking for internships</p>
+      <a href="/personal-works">Scroll down for personal work</a>
+    </div>
+  );
+}
+
+export default Home;

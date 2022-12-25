ce433928fb093ca7d1af297adb432f3a2d99fa52
jthach <jthach@iu.edu>
2022-12-25 11:16:52 -0800

    done

--- script.js
+++ script.js
@@ -26,4 +26,13 @@ function loadPage(url) {
       // When the HTML is received, update the main content area with the new content
       document.querySelector("main").innerHTML = html;
     });
+
+    window.onload = function() {
+        // Hide the loading screen
+        document.getElementById("loading").style.display = "none";
+      
+        // Show the main content
+        document.getElementById("content").style.display = "block";
+      };
+      
 }

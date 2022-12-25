364a18ea529db8e819cf2fc2e1fa37b5f3201768
jthach <jthach@iu.edu>
2022-12-25 11:06:38 -0800

    done

---
+++ script.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+  // Get all the nav links
+  var navLinks = document.querySelectorAll("nav a");
+
+  // Loop through the nav links and add a click event listener to each
+  navLinks.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      var target = event.target;
+      var href = target.getAttribute("href");
+
+      // Load the target page into the main content area
+      loadPage(href);
+    });
+  });
+}
+
+function loadPage(url) {
+  // Use fetch to make a GET request to the specified URL
+  fetch(url)
+    .then(function(response) {
+      // When the response is received, return it as text
+      return response.text();
+    })
+    .then(function(html) {
+      // When the HTML is received, update the main content area with the new content
+      document.querySelector("main").innerHTML = html;
+    });
+}

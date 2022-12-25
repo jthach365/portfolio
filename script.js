3818fd032fc202939083d13509b756d46b9ff5ae
jthach <jthach@iu.edu>
2022-12-25 12:04:11 -0800

    done

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
- // Get all the links in the navigation bar
- var navbarLinks = document.querySelectorAll(".navbar a");
+// Get all the links in the navigation bar
+var navbarLinks = document.querySelectorAll(".navbar a");
 
- // Loop through all the links
- for (var i = 0; i < navbarLinks.length; i++) {
-   // Add a click event listener to each link
-   navbarLinks[i].addEventListener("click", function() {
-     // Remove the active class from all the links
-     for (var i = 0; i < navbarLinks.length; i++) {
-       navbarLinks[i].classList.remove("active");
-     }
+// Loop through all the links
+for (var i = 0; i < navbarLinks.length; i++) {
+  // Add a click event listener to each link
+  navbarLinks[i].addEventListener("click", function() {
+    // Remove the active class from all the links
+    for (var i = 0; i < navbarLinks.length; i++) {
+      navbarLinks[i].classList.remove("active");
+    }
 
-     // Add the active class to the clicked link
-     this.classList.add("active");
+    // Add the active class to the clicked link
+    this.classList.add("active");
 
-     // Scroll to the corresponding section
-     var sectionId = this.getAttribute("href");
-     document.querySelector(sectionId).scrollIntoView({
-       behavior: "smooth"
-     });
-   });
- }
- 
+    // Scroll to the corresponding section
+    var sectionId = this.getAttribute("href");
+    document.querySelector(sectionId).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+}
